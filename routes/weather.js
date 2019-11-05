e3435978a0af759775e798b18c69c3877d4fee09
Ken Gil Romero <kenromero12345@gmail.com>
2019-11-05 13:32:20 -0800

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -11,17 +11,33 @@ router.use(bodyParser.json());
 //request module is needed to make a request to a web service
 const request = require('request');
 
-//TODO: does below need /weather/something
-router.post("/current", (req, res) => {
+
+router.post("/latLon", (req, res) => {
     // for info on use of tilde (`) making a String literal, see below. 
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
     let lat = req.body['lat'];
     let lon =  req.body['lon'];
     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPW_API_KEY}`;
-// `http:// api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${API_KEY}’;
-// `http:// api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY}’;
-    //res.send(url);
+
     //When this web service gets a request, make a request to the Phish Web service
+    request(url, function (error, _response, body) {
+        if (error) {
+            res.send(error);
+        } else {
+            // pass on everything (try out each of these in Postman to see the difference)
+            // res.send(response);
+            
+            // or just pass on the body
+            res.send(body);
+        }
+    });    
+});
+
+
+router.post("/zip", (req, res) => {
+    let zip = req.body['zip'];
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${OPW_API_KEY}`;
+
     request(url, function (error, _response, body) {
         if (error) {
             res.send(error);
