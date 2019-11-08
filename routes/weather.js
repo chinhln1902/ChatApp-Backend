a71d6bba97b0d2288974361de85fcace146a4d12
Ken Gil Romero <kenromero12345@gmail.com>
2019-11-07 19:03:18 -0800

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -25,7 +25,11 @@ router.post("/latLon", (req, res) => {
     //When this web service gets a request, make a request to the Phish Web service
     request(url, function (error, _response, body) {
         if (error) {
-            res.send(error);
+            res.send({
+                success: false,
+                errorMessage: "lat: " + lat + ", lon: " + lon + ", key: " + OPW_API_KEY + ", url: " + url,
+                error: error
+            });
         } else {
             // pass on everything (try out each of these in Postman to see the difference)
             // res.send(response);
