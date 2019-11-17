fa1b9074d204c1f257fdfb82f4d3e3e62e8a2e03
Ken Gil Romero <kenromero12345@gmail.com>
2019-11-16 16:32:00 -0800

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -146,8 +146,8 @@ router.post("/send", (req, res) => {
             " not supplied"
         });
     } else {
-        let insert = "INSERT INTO Locations (MemberID, Nickname) " //ZIP
-        + "SELECT MemberID, $2" //$5
+        let insert = "INSERT INTO Locations (MemberID, Nickname, Latitude, Longitude, Zip) " //ZIP
+        + "SELECT MemberID, $2, $3, $4, $5" 
         + "FROM Members WHERE email=$1 AND NOT EXISTS (SELECT * FROM MEMBERS JOIN LOCATIONS ON MEMBERS.MEMBERID = LOCATIONS.MEMBERID WHERE email = $1 AND nickname = $2)"
         // let insert = "INSERT INTO LOCATIONS (MEMBERID, NICKNAME) SELECT MEMBERID, $2 FROM MEMBERS WHERE email=$1 AND NOT EXISTS (SELECT * FROM MEMBERS JOIN LOCATIONS ON MEMBERS.MEMBERID = LOCATIONS.MEMBERID WHERE email = $a AND nickname = $2)"
         // let insert = "INSERT INTO Locations (MemberID, Nickname)" //ZIP
@@ -157,7 +157,7 @@ router.post("/send", (req, res) => {
         // +                        "FROM MEMBERS"
         // +                        "JOIN LOCATIONS ON MEMBERS.MEMBERID = LOCATIONS.MEMBERID"
         // +                        "WHERE email = $1 AND nickname = $2)"
-        db.none(insert, [email, city + ", " + country])//zip
+        db.none(insert, [email, city + ", " + country, lat, lon, zip])//zip
             .then(() => {
                 res.send({
                     success: true,
@@ -172,9 +172,6 @@ router.post("/send", (req, res) => {
                 });
             });
     }
-
-
-
 });
 
 router.post("/get", (req, res) => {
