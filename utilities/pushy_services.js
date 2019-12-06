c17955ec326b47007e11a1d5189e2da65e3d3f7d
chinhln1902 <chinh.ln1902@gmail.com>
2019-12-06 00:23:49 -0800

    changed pushy query

--- utilities/pushy_services.js
+++ utilities/pushy_services.js
@@ -2,14 +2,13 @@ var Pushy = require('pushy');
 // Plug in your Secret API Key
 var pushyAPI = new Pushy(process.env.PUSHY_API_KEY);
 //use to send message to all clients registered to a Topoic
-function sendToTopic(topic, msg, from, chatid, senderId) {
+function sendToTopic(topic, msg, from, chatid) {
     //build the message for FCM to send
     var data = {
         "type": "topic_msg",
         "sender": from,
         "message": msg,
-        "chatid": chatid,
-        "senderid": senderId
+        "chatid": chatid
     };
     console.log(data);
     to = '/topics/' + topic;
@@ -29,8 +28,8 @@ function sendToIndividual(token, msg, from, chatid, senderId) {
     //build the message for FCM to send
     var data = {
         "type": "msg",
-        "sender1": from,
-        "message1": msg,
+        "sender": from,
+        "message": msg,
         "chatid": chatid,
         "senderid": senderId
     };
